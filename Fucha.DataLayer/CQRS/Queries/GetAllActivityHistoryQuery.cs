2c2d12c4ac178ffec2fc9dcbd19f7f5d9c8cc56e
io-sono-roy <sabenecio.083905@laspinas.edu.ph>
2022-12-10 22:19:19 +0800

    cache

--- Fucha.DataLayer/CQRS/Queries/GetAllActivityHistoryQuery.cs
+++ Fucha.DataLayer/CQRS/Queries/GetAllActivityHistoryQuery.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Fucha.DataLayer.Models;
+using Fucha.DomainClasses;
+using MediatR;
 
 namespace Fucha.DataLayer.CQRS.Queries
 {
-    internal class GetAllActivityHistory
+    public class GetAllActivityHistoryQuery : IRequest<List<ActivityHistory>> { }
+    public class GetAllActivityHistoryQueryHandler : IRequestHandler<GetAllActivityHistoryQuery, List<ActivityHistory>>
     {
+        private readonly IFuchaMilkteaContext _context;
+
+        public GetAllActivityHistoryQueryHandler (IFuchaMilkteaContext context)
+        {
+            _context = context;
+        }
+
+        public Task<List<ActivityHistory>> Handle(GetAllActivityHistoryQuery request, CancellationToken cancellationToken)
+        {
+            var allActivityHistory = _context.ActivityHistories.Select(x => x).OrderByDescending(x => x.Id).ToList();
+
+            return Task.FromResult(allActivityHistory);
+        }
     }
+
 }
